cb7f9a28882443cbccc062bd69df0f5a49237e9c
antoniocalo <antonio.calo@nttdata.com>
2025-01-27 12:27:14 +0100

    P4ADEV-1852 using apache codec for hex calc

--- src/main/java/it/gov/pagopa/pu/send/util/FileUtils.java
+++ src/main/java/it/gov/pagopa/pu/send/util/FileUtils.java
@@ -3,8 +3,11 @@ package it.gov.pagopa.pu.send.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import org.apache.commons.codec.binary.Hex;
 
 public class FileUtils {
 
@@ -13,27 +16,11 @@ public class FileUtils {
 
   public static String calculateFileHash(File file)
     throws NoSuchAlgorithmException, IOException {
-    // Create a MessageDigest instance for SHA-256
     MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    // Create an InputStream to read the file
-    try (FileInputStream fis = new FileInputStream(file)) {
-      byte[] buffer = new byte[1024];
-      int bytesRead;
-
-      while ((bytesRead = fis.read(buffer)) != -1) {
-        digest.update(buffer, 0, bytesRead);
-      }
-    }
-    // Convert byte array into a hexadecimal string
-    byte[] hashBytes = digest.digest();
-    StringBuilder hexString = new StringBuilder();
-    for (byte b : hashBytes) {
-      String hex = Integer.toHexString(0xff & b);
-      if (hex.length() == 1) {
-        hexString.append('0');
-      }
-      hexString.append(hex);
-    }
-    return hexString.toString();
+    DigestInputStream digestInputStream = new DigestInputStream(new FileInputStream(file), digest);
+    byte[] inputStreamBuffer = new byte[8192];
+    while (digestInputStream.read(inputStreamBuffer) > -1);
+    byte[] hash = digestInputStream.getMessageDigest().digest();
+    return Hex.encodeHexString(hash);
   }
 }
