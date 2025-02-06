6530f236bf88d1ce5ddef765acf640af466133fe
antoniocalo <antonio.calo@nttdata.com>
2025-02-06 21:29:00 +0100

    P4ADEV-1854 handler for custom exception

--- src/main/java/it/gov/pagopa/pu/send/exception/ControllerExceptionHandler.java
+++ src/main/java/it/gov/pagopa/pu/send/exception/ControllerExceptionHandler.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
   @ExceptionHandler(InvalidStatusException.class)
   public ResponseEntity<String> handleInvalidStatusException(InvalidStatusException ex) {
     return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
   }
 }
