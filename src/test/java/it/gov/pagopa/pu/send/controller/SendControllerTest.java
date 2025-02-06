6c8b83f8499359830ee9c89c21bfed9d5cd007ca
antoniocalo <antonio.calo@nttdata.com>
2025-02-06 10:01:44 +0100

    P4ADEV-1854 add test

---
+++ src/test/java/it/gov/pagopa/pu/send/controller/SendControllerTest.java
@@ -0,0 +1,32 @@
+package it.gov.pagopa.pu.send.controller;
+
+import it.gov.pagopa.pu.send.service.SendService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class SendControllerTest {
+
+  @Mock
+  private SendService sendServiceMock;
+
+  @InjectMocks
+  private SendController sendController;
+
+  @Test
+  void givenSendNotificationIdWhenPreloadFilesRequestThenOk() {
+    String sendNotificationId = "12345";
+    Mockito.doNothing().when(sendServiceMock).preloadFiles(sendNotificationId);
+    ResponseEntity<Void> response = sendController.preloadSendFile(sendNotificationId);
+
+    Assertions.assertNotNull(response);
+    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+}
