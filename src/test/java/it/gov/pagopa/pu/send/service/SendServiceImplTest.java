948b313de34480ea33f97214a1757bbe9cbe9627
antoniocalo <antonio.calo@nttdata.com>
2025-02-06 12:00:03 +0100

    P4ADEV-1854 add test

---
+++ src/test/java/it/gov/pagopa/pu/send/service/SendServiceImplTest.java
@@ -0,0 +1,87 @@
+package it.gov.pagopa.pu.send.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+import it.gov.pagopa.pu.send.connector.client.SendClientImpl;
+import it.gov.pagopa.pu.send.connector.send.generated.dto.PreLoadRequestDTO;
+import it.gov.pagopa.pu.send.connector.send.generated.dto.PreLoadResponseDTO;
+import it.gov.pagopa.pu.send.connector.send.generated.dto.PreLoadResponseDTO.HttpMethodEnum;
+import it.gov.pagopa.pu.send.dto.DocumentDTO;
+import it.gov.pagopa.pu.send.enums.FileStatus;
+import it.gov.pagopa.pu.send.enums.NotificationStatus;
+import it.gov.pagopa.pu.send.model.SendNotification;
+import it.gov.pagopa.pu.send.repository.SendNotificationRepository;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SendServiceImplTest {
+  @Mock
+  private SendNotificationRepository sendNotificationRepository;
+
+  @Mock
+  private SendClientImpl sendClient;
+
+  @InjectMocks
+  private SendServiceImpl sendService;
+
+  @Test
+  void givenValidNotificationWhenPreloadFilesThenVerify() {
+    String sendNotificationId = "SENDNOTIFICATIONID";
+    String fileName = "FILENAME";
+
+    DocumentDTO documentDTO = DocumentDTO.builder()
+      .fileName(fileName)
+      .digest("digest123")
+      .contentType("application/pdf")
+      .status(FileStatus.READY)
+      .build();
+    SendNotification notification = SendNotification.builder()
+      .sendNotificationId(sendNotificationId)
+      .status(NotificationStatus.SENDING)
+      .documents(List.of(documentDTO))
+      .build();
+
+    PreLoadRequestDTO preLoadRequestDTO = new PreLoadRequestDTO();
+    preLoadRequestDTO.sha256("digest123");
+    preLoadRequestDTO.setContentType("application/pdf");
+    preLoadRequestDTO.setPreloadIdx(fileName);
+
+    PreLoadResponseDTO preLoadResponse = new PreLoadResponseDTO();
+    preLoadResponse.setPreloadIdx(fileName);
+    preLoadResponse.setKey("fileKey");
+    preLoadResponse.setSecret("fileSecret");
+    preLoadResponse.setHttpMethod(HttpMethodEnum.POST);
+    preLoadResponse.setUrl("http://example.com");
+
+    Mockito.when(sendNotificationRepository.findById(sendNotificationId)).thenReturn(
+      Optional.of(notification));
+
+    Mockito.when(sendClient.preloadFiles(List.of(preLoadRequestDTO))).thenReturn(List.of(preLoadResponse));
+
+    sendService.preloadFiles(sendNotificationId);
+
+    Mockito.verify(sendNotificationRepository, Mockito.times(1)).updateFilePreloadInformation(eq(sendNotificationId), any(PreLoadResponseDTO.class));
+    Mockito.verify(sendNotificationRepository, Mockito.times(1)).updateNotificationStatus(sendNotificationId, NotificationStatus.REGISTERED);
+  }
+
+  @Test
+  void givenNotExistsNotificationWhenPreloadFilesThenException() {
+    String sendNotificationId = "SENDNOTIFICATIONID";
+    Mockito.when(sendNotificationRepository.findById(sendNotificationId)).thenReturn(Optional.empty());
+
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      sendService.preloadFiles(sendNotificationId);
+    });
+
+    assertEquals("Notification not found with id: " + sendNotificationId, exception.getMessage());
+  }
+}
