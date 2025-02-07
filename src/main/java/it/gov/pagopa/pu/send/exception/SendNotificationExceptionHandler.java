5ed4107730409c298e940c5b46980a083705c459
antonio.torre <antonio.torre@pagopa.it>
2025-02-07 15:01:47 +0100

    fix: handling error URL not existent

--- src/main/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandler.java
+++ src/main/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandler.java
@@ -12,9 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.stream.Collectors;
 
@@ -22,83 +24,88 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SendNotificationExceptionHandler {
 
-    @ExceptionHandler({ValidationException.class, HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<SendNotificationErrorDTO> handleViolationException(Exception ex, HttpServletRequest request) {
-        return handleException(ex, request, HttpStatus.BAD_REQUEST, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
+  @ExceptionHandler({ValidationException.class, HttpMessageNotReadableException.class, MethodArgumentNotValidException.class, MethodArgumentTypeMismatchException.class})
+  public ResponseEntity<SendNotificationErrorDTO> handleViolationException(Exception ex, HttpServletRequest request) {
+    return handleException(ex, request, HttpStatus.BAD_REQUEST, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
+  }
+
+  @ExceptionHandler({ServletException.class, ErrorResponseException.class})
+  public ResponseEntity<SendNotificationErrorDTO> handleServletException(Exception ex, HttpServletRequest request) {
+    HttpStatusCode httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    SendNotificationErrorDTO.CodeEnum errorCode = SendNotificationErrorDTO.CodeEnum.GENERIC_ERROR;
+    if (ex instanceof ErrorResponse errorResponse) {
+      httpStatus = errorResponse.getStatusCode();
+      if (httpStatus.isSameCodeAs(HttpStatus.NOT_FOUND)) {
+        errorCode = SendNotificationErrorDTO.CodeEnum.NOT_FOUND;
+      } else if (httpStatus.is4xxClientError()) {
+        errorCode = SendNotificationErrorDTO.CodeEnum.BAD_REQUEST;
+      }
     }
+    return handleException(ex, request, httpStatus, errorCode);
+  }
 
-    @ExceptionHandler({ServletException.class})
-    public ResponseEntity<SendNotificationErrorDTO> handleServletException(ServletException ex, HttpServletRequest request) {
-        HttpStatusCode httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        SendNotificationErrorDTO.CodeEnum errorCode = SendNotificationErrorDTO.CodeEnum.GENERIC_ERROR;
-        if (ex instanceof ErrorResponse errorResponse) {
-            httpStatus = errorResponse.getStatusCode();
-            if (httpStatus.is4xxClientError()) {
-                errorCode = SendNotificationErrorDTO.CodeEnum.BAD_REQUEST;
-            }
-        }
-        return handleException(ex, request, httpStatus, errorCode);
+  @ExceptionHandler({RuntimeException.class})
+  public ResponseEntity<SendNotificationErrorDTO> handleRuntimeException(RuntimeException ex, HttpServletRequest request) {
+    return handleException(ex, request, HttpStatus.INTERNAL_SERVER_ERROR, SendNotificationErrorDTO.CodeEnum.GENERIC_ERROR);
+  }
+
+  @ExceptionHandler(InvalidStatusException.class)
+  public ResponseEntity<SendNotificationErrorDTO> handleInvalidStatusException(InvalidStatusException ex, HttpServletRequest request) {
+    return handleException(ex, request, HttpStatus.CONFLICT, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<SendNotificationErrorDTO> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
+    return handleException(ex, request, HttpStatus.NOT_FOUND, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
+  }
+
+  static ResponseEntity<SendNotificationErrorDTO> handleException(Exception ex, HttpServletRequest request, HttpStatusCode httpStatus, SendNotificationErrorDTO.CodeEnum errorEnum) {
+    logException(ex, request, httpStatus);
+
+    String message = buildReturnedMessage(ex);
+
+    return ResponseEntity
+      .status(httpStatus)
+      .body(new SendNotificationErrorDTO(errorEnum, message));
+  }
+
+  private static void logException(Exception ex, HttpServletRequest request, HttpStatusCode httpStatus) {
+    log.info("A {} occurred handling request {}: HttpStatus {} - {}",
+      ex.getClass(),
+      getRequestDetails(request),
+      httpStatus.value(),
+      ex.getMessage());
+    if (log.isDebugEnabled() && ex.getCause() != null) {
+      log.debug("CausedBy: ", ex.getCause());
     }
+  }
 
-    @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<SendNotificationErrorDTO> handleRuntimeException(RuntimeException ex, HttpServletRequest request) {
-        return handleException(ex, request, HttpStatus.INTERNAL_SERVER_ERROR, SendNotificationErrorDTO.CodeEnum.GENERIC_ERROR);
+  private static String buildReturnedMessage(Exception ex) {
+    if (ex instanceof HttpMessageNotReadableException) {
+      if (ex.getCause() instanceof JsonMappingException jsonMappingException) {
+        return "Cannot parse body: " +
+          jsonMappingException.getPath().stream()
+            .map(JsonMappingException.Reference::getFieldName)
+            .collect(Collectors.joining(".")) +
+          ": " + jsonMappingException.getOriginalMessage();
+      }
+      return "Required request body is missing";
+    } else if (ex instanceof MethodArgumentNotValidException methodArgumentNotValidException) {
+      return "Invalid request content:" +
+        methodArgumentNotValidException.getBindingResult()
+          .getAllErrors().stream()
+          .map(e -> " " +
+            (e instanceof FieldError fieldError ? fieldError.getField() : e.getObjectName()) +
+            ": " + e.getDefaultMessage())
+          .sorted()
+          .collect(Collectors.joining(";"));
+    } else {
+      return ex.getMessage();
     }
+  }
 
-    @ExceptionHandler(InvalidStatusException.class)
-    public ResponseEntity<SendNotificationErrorDTO> handleInvalidStatusException(InvalidStatusException ex, HttpServletRequest request) {
-      return handleException(ex, request, HttpStatus.CONFLICT, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<SendNotificationErrorDTO> handleIllegalArgumentException(IllegalArgumentException ex, HttpServletRequest request) {
-      return handleException(ex, request, HttpStatus.NOT_FOUND, SendNotificationErrorDTO.CodeEnum.BAD_REQUEST);
-    }
-
-    static ResponseEntity<SendNotificationErrorDTO> handleException(Exception ex, HttpServletRequest request, HttpStatusCode httpStatus, SendNotificationErrorDTO.CodeEnum errorEnum) {
-        logException(ex, request, httpStatus);
-
-        String message = buildReturnedMessage(ex);
-
-        return ResponseEntity
-                .status(httpStatus)
-                .body(new SendNotificationErrorDTO(errorEnum, message));
-    }
-
-    private static void logException(Exception ex, HttpServletRequest request, HttpStatusCode httpStatus) {
-        log.info("A {} occurred handling request {}: HttpStatus {} - {}",
-                ex.getClass(),
-                getRequestDetails(request),
-                httpStatus.value(),
-                ex.getMessage());
-    }
-
-    private static String buildReturnedMessage(Exception ex) {
-        if (ex instanceof HttpMessageNotReadableException) {
-            if(ex.getCause() instanceof JsonMappingException jsonMappingException){
-                return "Cannot parse body: " +
-                        jsonMappingException.getPath().stream()
-                                .map(JsonMappingException.Reference::getFieldName)
-                                .collect(Collectors.joining(".")) +
-                        ": " + jsonMappingException.getOriginalMessage();
-            }
-            return "Required request body is missing";
-        } else if (ex instanceof MethodArgumentNotValidException methodArgumentNotValidException) {
-            return "Invalid request content:" +
-                    methodArgumentNotValidException.getBindingResult()
-                            .getAllErrors().stream()
-                            .map(e -> " " +
-                                    (e instanceof FieldError fieldError? fieldError.getField(): e.getObjectName()) +
-                                    ": " + e.getDefaultMessage())
-                            .sorted()
-                            .collect(Collectors.joining(";"));
-        } else {
-            return ex.getMessage();
-        }
-    }
-
-    static String getRequestDetails(HttpServletRequest request) {
-        return "%s %s".formatted(request.getMethod(), request.getRequestURI());
-    }
+  static String getRequestDetails(HttpServletRequest request) {
+    return "%s %s".formatted(request.getMethod(), request.getRequestURI());
+  }
 
 }
--- src/test/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandlerTest.java
@@ -8,7 +8,6 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import java.lang.reflect.Field;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -43,166 +42,174 @@ import static org.mockito.Mockito.doThrow;
 @ExtendWith({SpringExtension.class})
 @WebMvcTest(value = {SendNotificationExceptionHandlerTest.TestController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @ContextConfiguration(classes = {
-        SendNotificationExceptionHandlerTest.TestController.class,
-        SendNotificationExceptionHandler.class,
-        MongoTooManyRequestsExceptionHandler.class,
-        JsonConfig.class})
+  SendNotificationExceptionHandlerTest.TestController.class,
+  SendNotificationExceptionHandler.class,
+  MongoTooManyRequestsExceptionHandler.class,
+  JsonConfig.class})
 class SendNotificationExceptionHandlerTest {
 
-    public static final String DATA = "data";
-    public static final TestRequestBody BODY = new TestRequestBody("bodyData", null, "abc", LocalDateTime.now());
+  public static final String DATA = "data";
+  public static final TestRequestBody BODY = new TestRequestBody("bodyData", null, "abc", LocalDateTime.now());
 
-    @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
 
-    @MockitoSpyBean
-    private TestController testControllerSpy;
-    @MockitoSpyBean
-    private RequestMappingHandlerAdapter requestMappingHandlerAdapterSpy;
+  @MockitoSpyBean
+  private TestController testControllerSpy;
+  @MockitoSpyBean
+  private RequestMappingHandlerAdapter requestMappingHandlerAdapterSpy;
 
-    @RestController
-    @Slf4j
-    static class TestController {
-        @PostMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
-        String testEndpoint(@RequestParam(DATA) String data, @Valid @RequestBody TestRequestBody body) {
-            return "OK";
-        }
+  @RestController
+  @Slf4j
+  static class TestController {
+    @PostMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    String testEndpoint(@RequestParam(DATA) String data, @Valid @RequestBody TestRequestBody body) {
+      return "OK";
+    }
+  }
+
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class TestRequestBody {
+    @NotNull
+    private String requiredField;
+    private String notRequiredField;
+    @Pattern(regexp = "[a-z]+")
+    private String lowerCaseAlphabeticField;
+    private LocalDateTime dateTimeField;
+  }
+
+  private ResultActions performRequest(String data, MediaType accept) throws Exception {
+    return performRequest(data, accept, objectMapper.writeValueAsString(SendNotificationExceptionHandlerTest.BODY));
+  }
+
+  private ResultActions performRequest(String data, MediaType accept, String body) throws Exception {
+    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/test")
+      .param(DATA, data)
+      .accept(accept);
+
+    if (body != null) {
+      requestBuilder
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(body);
     }
 
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class TestRequestBody {
-        @NotNull
-        private String requiredField;
-        private String notRequiredField;
-        @Pattern(regexp = "[a-z]+")
-        private String lowerCaseAlphabeticField;
-        private LocalDateTime dateTimeField;
-    }
+    return mockMvc.perform(requestBuilder);
+  }
 
-    private ResultActions performRequest(String data, MediaType accept) throws Exception {
-        return performRequest(data, accept, objectMapper.writeValueAsString(SendNotificationExceptionHandlerTest.BODY));
-    }
+  @Test
+  void handleMissingServletRequestParameterException() throws Exception {
+    performRequest(null, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request parameter 'data' for method parameter type String is not present"));
 
-    private ResultActions performRequest(String data, MediaType accept, String body) throws Exception {
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/test")
-                .param(DATA, data)
-                .accept(accept);
+  }
 
-        if (body != null) {
-            requestBuilder
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(body);
-        }
+  @Test
+  void handleRuntimeExceptionError() throws Exception {
+    doThrow(new RuntimeException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
 
-        return mockMvc.perform(requestBuilder);
-    }
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+  }
 
-    @Test
-    void handleMissingServletRequestParameterException() throws Exception {
-        performRequest(null, MediaType.APPLICATION_JSON)
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request parameter 'data' for method parameter type String is not present"));
+  @Test
+  void handleGenericServletException() throws Exception {
+    doThrow(new ServletException("Error"))
+      .when(requestMappingHandlerAdapterSpy).handle(any(), any(), any());
 
-    }
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+  }
 
-    @Test
-    void handleRuntimeExceptionError() throws Exception {
-        doThrow(new RuntimeException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
+  @Test
+  void handle4xxHttpServletException() throws Exception {
+    performRequest(DATA, MediaType.parseMediaType("application/hal+json"))
+      .andExpect(MockMvcResultMatchers.status().isNotAcceptable())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No acceptable representation"));
+  }
 
-        performRequest(DATA, MediaType.APPLICATION_JSON)
-                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
-    }
+  @Test
+  void handleUrlNotFound() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.post("/NOTEXISTENTURL"))
+      .andExpect(MockMvcResultMatchers.status().isNotFound())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_NOT_FOUND"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL."));
+  }
 
-    @Test
-    void handleGenericServletException() throws Exception {
-        doThrow(new ServletException("Error"))
-                .when(requestMappingHandlerAdapterSpy).handle(any(), any(), any());
+  @Test
+  void handleNoBodyException() throws Exception {
+    performRequest(DATA, MediaType.APPLICATION_JSON, null)
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request body is missing"));
+  }
 
-        performRequest(DATA, MediaType.APPLICATION_JSON)
-                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
-    }
+  @Test
+  void handleInvalidBodyException() throws Exception {
+    performRequest(DATA, MediaType.APPLICATION_JSON,
+      "{\"notRequiredField\":\"notRequired\",\"lowerCaseAlphabeticField\":\"ABC\"}")
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content: lowerCaseAlphabeticField: must match \"[a-z]+\"; requiredField: must not be null"));
+  }
 
-    @Test
-    void handle4xxHttpServletException() throws Exception {
-        performRequest(DATA, MediaType.parseMediaType("application/hal+json"))
-                .andExpect(MockMvcResultMatchers.status().isNotAcceptable())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No acceptable representation"));
-    }
+  @Test
+  void handleNotParsableBodyException() throws Exception {
+    performRequest(DATA, MediaType.APPLICATION_JSON,
+      "{\"notRequiredField\":\"notRequired\",\"dateTimeField\":\"2025-02-05\"}")
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Cannot parse body: dateTimeField: Text '2025-02-05' could not be parsed at index 10"));
+  }
 
-    @Test
-    void handleNoBodyException() throws Exception {
-        performRequest(DATA, MediaType.APPLICATION_JSON, null)
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Required request body is missing"));
-    }
+  @Test
+  void handle5xxHttpServletException() throws Exception {
+    doThrow(new ServerErrorException("Error", new RuntimeException("Error")))
+      .when(requestMappingHandlerAdapterSpy).handle(any(), any(), any());
 
-    @Test
-    void handleInvalidBodyException() throws Exception {
-        performRequest(DATA, MediaType.APPLICATION_JSON,
-                "{\"notRequiredField\":\"notRequired\",\"lowerCaseAlphabeticField\":\"ABC\"}")
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Invalid request content: lowerCaseAlphabeticField: must match \"[a-z]+\"; requiredField: must not be null"));
-    }
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("500 INTERNAL_SERVER_ERROR \"Error\""));
+  }
 
-    @Test
-    void handleNotParsableBodyException() throws Exception {
-        performRequest(DATA, MediaType.APPLICATION_JSON,
-                "{\"notRequiredField\":\"notRequired\",\"dateTimeField\":\"2025-02-05\"}")
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Cannot parse body: dateTimeField: Text '2025-02-05' could not be parsed at index 10"));
-    }
+  @Test
+  void handleViolationException() throws Exception {
+    doThrow(new ConstraintViolationException("Error", Set.of())).when(testControllerSpy).testEndpoint(DATA, BODY);
 
-    @Test
-    void handle5xxHttpServletException() throws Exception {
-        doThrow(new ServerErrorException("Error", new RuntimeException("Error")))
-                .when(requestMappingHandlerAdapterSpy).handle(any(), any(), any());
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+  }
 
-        performRequest(DATA, MediaType.APPLICATION_JSON)
-                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_GENERIC_ERROR"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("500 INTERNAL_SERVER_ERROR \"Error\""));
-    }
+  @Test
+  void handleInvalidStatusException() throws Exception {
+    doThrow(new InvalidStatusException(NotificationStatus.WAITING_FILE, NotificationStatus.SENDING)).when(testControllerSpy).testEndpoint(DATA, BODY);
 
-    @Test
-    void handleViolationException() throws Exception {
-        doThrow(new ConstraintViolationException("Error", Set.of())).when(testControllerSpy).testEndpoint(DATA, BODY);
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isConflict())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Notification status error: Expected: WAITING_FILE, Actual: SENDING"));
+  }
 
-        performRequest(DATA, MediaType.APPLICATION_JSON)
-                .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
-    }
+  @Test
+  void handleIllegalArgumentException() throws Exception {
+    doThrow(new IllegalArgumentException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
 
-    @Test
-    void handleInvalidStatusException() throws Exception {
-        doThrow(new InvalidStatusException(NotificationStatus.WAITING_FILE, NotificationStatus.SENDING)).when(testControllerSpy).testEndpoint(DATA, BODY);
-
-        performRequest(DATA, MediaType.APPLICATION_JSON)
-          .andExpect(MockMvcResultMatchers.status().isConflict())
-          .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-          .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Notification status error: Expected: WAITING_FILE, Actual: SENDING"));
-      }
-
-    @Test
-    void handleIllegalArgumentException() throws Exception {
-      doThrow(new IllegalArgumentException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
-
-      performRequest(DATA, MediaType.APPLICATION_JSON)
-        .andExpect(MockMvcResultMatchers.status().isNotFound())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
-      }
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isNotFound())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+  }
 }
