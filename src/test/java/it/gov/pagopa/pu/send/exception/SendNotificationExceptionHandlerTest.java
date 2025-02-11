0bc966d614c7ef5016e99f1602d44fc822c67463
antoniocalo <antonio.calo@nttdata.com>
2025-02-11 15:50:31 +0100

    P4ADEV-1855 implements tests

--- src/test/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/send/exception/SendNotificationExceptionHandlerTest.java
@@ -212,4 +212,14 @@ class SendNotificationExceptionHandlerTest {
       .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
       .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
   }
+
+  @Test
+  void handleInvalidSignatureException() throws Exception {
+    doThrow(new InvalidSignatureException("Error")).when(testControllerSpy).testEndpoint(DATA, BODY);
+
+    performRequest(DATA, MediaType.APPLICATION_JSON)
+      .andExpect(MockMvcResultMatchers.status().isBadRequest())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("SEND_NOTIFICATION_BAD_REQUEST"))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Error"));
+  }
 }
