02347bab373a8277fac2cd68c3e47a2d4ee4393c
antoniocalo <antonio.calo@nttdata.com>
2025-02-17 18:00:45 +0100

    P4ADEV-1861 implements SEND status API

--- src/main/java/it/gov/pagopa/pu/send/connector/pagopa/send/config/PagopaSendApisHolder.java
+++ src/main/java/it/gov/pagopa/pu/send/connector/pagopa/send/config/PagopaSendApisHolder.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.send.connector.pagopa.send.config;
 import it.gov.pagopa.pu.send.config.RestTemplateConfig;
 import it.gov.pagopa.pu.send.connector.send.generated.ApiClient;
 import it.gov.pagopa.pu.send.connector.send.generated.api.NewNotificationApi;
+import it.gov.pagopa.pu.send.connector.send.generated.api.SenderReadB2BApi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ public class PagopaSendApisHolder {
   private final PagopaSendApiClientConfig clientConfig;
 
   private final Map<String, NewNotificationApi> newNotificationApiMap = new ConcurrentHashMap<>();
+  private final Map<String, SenderReadB2BApi> senderReadB2BApiMap = new ConcurrentHashMap<>();
 
   public PagopaSendApisHolder(
     PagopaSendApiClientConfig clientConfig,
@@ -42,4 +44,14 @@ public class PagopaSendApisHolder {
     });
   }
 
+  public SenderReadB2BApi getSenderReadB2BApiByApiKey(String apiKey) {
+    return senderReadB2BApiMap.computeIfAbsent(apiKey, key -> {
+      ApiClient apiClient = new ApiClient(restTemplate);
+      apiClient.setBasePath(clientConfig.getBaseUrl());
+      apiClient.setApiKey(key);
+      apiClient.setMaxAttemptsForRetry(Math.max(1, clientConfig.getMaxAttempts()));
+      apiClient.setWaitTimeMillis(clientConfig.getWaitTimeMillis());
+      return new SenderReadB2BApi(apiClient);
+    });
+  }
 }
