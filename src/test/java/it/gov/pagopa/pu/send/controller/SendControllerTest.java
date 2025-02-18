eef3774770b86792e43960c4c55f9700f22fb5a7
antoniocalo <antonio.calo@nttdata.com>
2025-02-18 10:54:21 +0100

    P4ADEV-1861 implements test

--- src/test/java/it/gov/pagopa/pu/send/controller/SendControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/send/controller/SendControllerTest.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.send.controller;
 
+import it.gov.pagopa.pu.send.connector.send.generated.dto.NewNotificationRequestStatusResponseV24DTO;
 import it.gov.pagopa.pu.send.service.SendFacadeService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -48,5 +49,18 @@ class SendControllerTest {
 
     Assertions.assertNotNull(response);
     Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+  }
+
+  @Test
+  void givenSendNotificationIdWhenNotificationStatusRequestThenOk(){
+    String sendNotificationId = "12345";
+    NewNotificationRequestStatusResponseV24DTO status = new NewNotificationRequestStatusResponseV24DTO();
+    Mockito.when(sendFacadeServiceMock.notificationStatus(sendNotificationId)).thenReturn(status);
+
+    ResponseEntity<NewNotificationRequestStatusResponseV24DTO> response = sendController.notificationStatus(sendNotificationId);
+
+    Assertions.assertNotNull(response);
+    Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    Assertions.assertEquals(status, response.getBody());
   }
 }
