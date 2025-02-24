0ffe1f590955ffc75bb9463d651eb018237470f5
antoniocalo <antonio.calo@nttdata.com>
2025-02-24 12:00:45 +0100

    P4ADEV-1859 resolve issues

--- src/test/java/it/gov/pagopa/pu/send/service/SendNotificationServiceImplTest.java
+++ src/test/java/it/gov/pagopa/pu/send/service/SendNotificationServiceImplTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
