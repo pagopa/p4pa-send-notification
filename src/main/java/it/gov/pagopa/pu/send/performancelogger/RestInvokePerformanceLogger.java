8f2910d4e98124f27e9b51913463e6699a7df38a
antonio.torre <antonio.torre@pagopa.it>
2025-03-03 13:26:31 +0100

    fix: PII in REST_INVOKE logs

--- src/main/java/it/gov/pagopa/pu/send/performancelogger/RestInvokePerformanceLogger.java
+++ src/main/java/it/gov/pagopa/pu/send/performancelogger/RestInvokePerformanceLogger.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.pu.send.performancelogger;
 
+import it.gov.pagopa.pu.send.util.SecurityUtils;
 import jakarta.annotation.Nonnull;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -23,6 +24,6 @@ public class RestInvokePerformanceLogger implements ClientHttpRequestInterceptor
     }
 
     static String getRequestDetails(HttpRequest request) {
-        return "%s %s".formatted(request.getMethod(), request.getURI());
+        return "%s %s".formatted(request.getMethod(), SecurityUtils.removePiiFromURI(request.getURI()));
     }
 }
