2d3ba53c753e8e7681f4d412150e85b5277be129
antonio.torre <antonio.torre@pagopa.it>
2025-03-11 18:50:46 +0100

    feat: Configuring build-info on actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
-	jacoco
-	id("org.sonarqube") version "6.0.1.5171"
-	id("com.github.ben-manes.versions") version "0.51.0"
-	id("org.openapi.generator") version "7.10.0"
+  java
+  id("org.springframework.boot") version "3.4.3"
+  id("io.spring.dependency-management") version "1.1.7"
+  jacoco
+  id("org.sonarqube") version "6.0.1.5171"
+  id("com.github.ben-manes.versions") version "0.51.0"
+  id("org.openapi.generator") version "7.10.0"
   id("org.ajoberstar.grgit") version "5.3.0"
+  id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -16,19 +17,19 @@ version = "0.0.1"
 description = "p4pa-send-notification"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 val springDocOpenApiVersion = "2.8.5"
@@ -37,32 +38,32 @@ val micrometerVersion = "1.4.3"
 val bouncycastleVersion = "1.80"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
   implementation("io.micrometer:micrometer-tracing-bridge-otel:$micrometerVersion")
   implementation("io.micrometer:micrometer-registry-prometheus")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-	implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocOpenApiVersion")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+  implementation("org.openapitools:jackson-databind-nullable:$openApiToolsVersion")
   implementation("org.bouncycastle:bcprov-jdk18on:$bouncycastleVersion")
 
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+  compileOnly("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
   testAnnotationProcessor("org.projectlombok:lombok")
 
   //	Testing
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.mockito:mockito-core")
-	testImplementation ("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.mockito:mockito-core")
+  testImplementation("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	finalizedBy(tasks.jacocoTestReport)
+  useJUnitPlatform()
+  finalizedBy(tasks.jacocoTestReport)
 }
 
 val mockitoAgent = configurations.create("mockitoAgent")
@@ -76,29 +77,29 @@ tasks {
 }
 
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-	reports {
-		xml.required = true
-	}
+  dependsOn(tasks.test)
+  reports {
+    xml.required = true
+  }
 }
 
 val projectInfo = mapOf(
-	"artifactId" to project.name,
-	"version" to project.version
+  "artifactId" to project.name,
+  "version" to project.version
 )
 
 tasks {
-	val processResources by getting(ProcessResources::class) {
-		filesMatching("**/application.yml") {
-			expand(projectInfo)
-		}
-	}
+  val processResources by getting(ProcessResources::class) {
+    filesMatching("**/application.yml") {
+      expand(projectInfo)
+    }
+  }
 }
 
 configurations {
-	compileClasspath {
-		resolutionStrategy.activateDependencyLocking()
-	}
+  compileClasspath {
+    resolutionStrategy.activateDependencyLocking()
+  }
 }
 
 tasks.compileJava {
@@ -117,13 +118,14 @@ tasks.register("dependenciesBuild") {
 }
 
 configure<SourceSetContainer> {
-	named("main") {
-		java.srcDir("$projectDir/build/generated/src/main/java")
-	}
+  named("main") {
+    java.srcDir("$projectDir/build/generated/src/main/java")
+  }
 }
 
 springBoot {
-	mainClass.value("it.gov.pagopa.pu.send.SendNotificationApplication")
+  buildInfo()
+  mainClass.value("it.gov.pagopa.pu.send.SendNotificationApplication")
 }
 
 tasks.register<GenerateTask>("openApiGenerateP4PASend") {
