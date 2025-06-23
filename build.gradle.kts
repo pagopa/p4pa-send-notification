a82d2bf117dec7917138861ed97d653855279f42
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-06-23 13:02:16 +0200

    fix: P4ADEV-3193 fix cve (#66)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import java.util.*
 
 plugins {
   java
-  id("org.springframework.boot") version "3.5.0"
+  id("org.springframework.boot") version "3.5.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "6.1.0.5360"
+  id("org.sonarqube") version "6.2.0.5505"
   id("com.github.ben-manes.versions") version "0.52.0"
   id("org.openapi.generator") version "7.13.0"
-  id("org.ajoberstar.grgit") version "5.3.0"
+  id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
@@ -39,7 +39,7 @@ val micrometerVersion = "1.5.1"
 val bouncycastleVersion = "1.81"
 val httpClientVersion = "5.5"
 val postgresJdbcVersion = "42.7.7"
-val caffeineVersion = "3.2.0"
+val caffeineVersion = "3.2.1"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter")
