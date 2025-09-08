f30209fc1c076f9a0ff0005829fbe33d189453af
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2025-09-08 10:35:23 +0200

    P4ADEV-3732-fetch-legal-fact-list-from-send implemented modify GET on /send/{sendNotificationId}/legal-facts, removing hashing for TaxId, and adding null check on LegalFactId in mapper

--- src/main/java/it/gov/pagopa/pu/send/mapper/SendLegalFactMapper.java
+++ src/main/java/it/gov/pagopa/pu/send/mapper/SendLegalFactMapper.java
@@ -1,35 +1,26 @@
 package it.gov.pagopa.pu.send.mapper;
 
-import it.gov.pagopa.pu.send.citizen.service.DataCipherService;
 import it.gov.pagopa.pu.send.connector.send.generated.dto.LegalFactListElementV20DTO;
 import it.gov.pagopa.pu.send.connector.send.generated.dto.LegalFactsIdV20DTO;
 import it.gov.pagopa.pu.send.dto.generated.LegalFactListElementDTO;
 import it.gov.pagopa.pu.send.dto.generated.LegalFactIdDTO;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class SendLegalFactMapper {
-
-  private final DataCipherService dataCipherService;
-
-  public SendLegalFactMapper(DataCipherService dataCipherService) {
-    this.dataCipherService = dataCipherService;
-  }
 
   public LegalFactListElementDTO mapLegalFactDTOFromSend(LegalFactListElementV20DTO legalFactListElementSendDto) {
     return LegalFactListElementDTO.builder()
       .iun(legalFactListElementSendDto.getIun())
-      .taxId(
-        Optional.ofNullable(legalFactListElementSendDto.getTaxId())
-          .map(dataCipherService::hash)
-          .orElse(null)
-      ).legalFactId(this.mapLegalFactIdDTOFromSend(legalFactListElementSendDto.getLegalFactsId()))
+      .taxId(legalFactListElementSendDto.getTaxId())
+      .legalFactId(this.mapLegalFactIdDTOFromSend(legalFactListElementSendDto.getLegalFactsId()))
       .build();
   }
 
   private LegalFactIdDTO mapLegalFactIdDTOFromSend(LegalFactsIdV20DTO legalFactIdSendDto) {
+    if(legalFactIdSendDto == null) {
+      return null;
+    }
     return LegalFactIdDTO.builder()
         .key(legalFactIdSendDto.getKey())
         .category(legalFactIdSendDto.getCategory())
