61de790a1f72d2732d2770f41270a5283cd198f9
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2025-09-09 16:08:22 +0200

    P4ADEV-3733-fetch-legal-fact-download-metadata-from-send fixed mapping of legalFactId.key in fetch Legal Fact list from send (removing unuseful prefix)

--- src/main/java/it/gov/pagopa/pu/send/mapper/SendLegalFactMapper.java
+++ src/main/java/it/gov/pagopa/pu/send/mapper/SendLegalFactMapper.java
@@ -6,10 +6,17 @@ import it.gov.pagopa.pu.send.dto.generated.LegalFactListElementDTO;
 import it.gov.pagopa.pu.send.dto.generated.LegalFactIdDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import static it.gov.pagopa.pu.send.util.Constants.LEGAL_FACT_ID_PREFIX;
+
 @Service
 public class SendLegalFactMapper {
 
   public LegalFactListElementDTO mapLegalFactDTOFromSend(LegalFactListElementV20DTO legalFactListElementSendDto) {
+    if(legalFactListElementSendDto == null) {
+      return null;
+    }
     return LegalFactListElementDTO.builder()
       .iun(legalFactListElementSendDto.getIun())
       .taxId(legalFactListElementSendDto.getTaxId())
@@ -22,8 +29,15 @@ public class SendLegalFactMapper {
       return null;
     }
     return LegalFactIdDTO.builder()
-        .key(legalFactIdSendDto.getKey())
+        .key(this.mapLegalFactIdKey(legalFactIdSendDto.getKey()))
         .category(legalFactIdSendDto.getCategory())
         .build();
   }
+
+  private String mapLegalFactIdKey(String legalFactIdKey) {
+    return Optional.ofNullable(legalFactIdKey)
+      .map(key -> key.replace(LEGAL_FACT_ID_PREFIX, ""))
+      .orElse(null);
+  }
+
 }
