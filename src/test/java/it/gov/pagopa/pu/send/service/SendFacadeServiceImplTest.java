244b09fbfe5af8d4a7a72eb2857283d3668e0c7a
antocalo <antonio.calo@nttdata.com>
2026-03-18 09:31:55 +0100

    P4ADEV-4524-change-update-dataRetrieve

--- src/test/java/it/gov/pagopa/pu/send/service/SendFacadeServiceImplTest.java
+++ src/test/java/it/gov/pagopa/pu/send/service/SendFacadeServiceImplTest.java
@@ -35,7 +35,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.HttpClientErrorException;
