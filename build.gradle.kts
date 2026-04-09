b2e76533d95ab0382c996fadfc2c467b9bc14b99
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2026-04-09 17:22:53 +0200

    fix: Fix CVE (#144)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.4"
+  id("org.springframework.boot") version "4.0.5"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
   id("org.sonarqube") version "7.2.3.7755"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.20.0"
+  id("org.openapi.generator") version "7.21.0"
   id("org.ajoberstar.grgit") version "5.3.2"
   id("com.gorylenko.gradle-git-properties") version "2.5.7"
-  id("com.github.jk1.dependency-license-report") version "3.1.1"
+  id("com.github.jk1.dependency-license-report") version "3.1.2"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -53,18 +53,17 @@ repositories {
 
 val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.9"
-val micrometerVersion = "1.6.3"
+val openApiToolsVersion = "0.2.10"
+val micrometerVersion = "1.6.4"
 val bouncycastleVersion = "1.83"
 val httpClientVersion = "5.6"
-val httpCoreVersion = "5.4.1"
+val httpCoreVersion = "5.4.2"
 val postgresJdbcVersion = "42.7.10"
 val caffeineVersion = "3.2.3"
 val commonsLang3Version = "3.20.0"
 val podamVersion = "8.0.2.RELEASE"
 
 // fix cve
-val jackson2CoreVersion = "2.21.1"
 val jackson3CoreVersion = "3.1.1"
 
 dependencies {
@@ -93,7 +92,6 @@ dependencies {
 
   // CVE fix
   implementation("tools.jackson.core:jackson-core:$jackson3CoreVersion")
-  implementation("com.fasterxml.jackson.core:jackson-core:$jackson2CoreVersion")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
