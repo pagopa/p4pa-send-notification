8fd0ec02eac768bd7f7b1b26c282081c18a1625e
antocalo <antonio.calo@nttdata.com>
2026-05-19 17:50:34 +0200

    P4ADEV-4659 implements tests

---
+++ src/test/java/it/gov/pagopa/pu/send/service/TaxonomyValidatorServiceImplTest.java
@@ -0,0 +1,95 @@
+package it.gov.pagopa.pu.send.service;
+
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.send.connector.organization.service.OrganizationService;
+import it.gov.pagopa.pu.send.exception.InvalidTaxonomyException;
+import it.gov.pagopa.pu.send.exception.NotFoundException;
+import it.gov.pagopa.pu.send.model.SendTaxonomy;
+import it.gov.pagopa.pu.send.repository.SendTaxonomyRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaxonomyValidatorServiceImplTest {
+  @Mock
+  private SendTaxonomyRepository sendTaxonomyRepositoryMock;
+  @Mock
+  private OrganizationService organizationServiceMock;
+
+  @InjectMocks
+  private TaxonomyValidatorServiceImpl service;
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(sendTaxonomyRepositoryMock, organizationServiceMock);
+  }
+
+  @Test
+  void givenValidTaxonomyCodeForOrganizationWhenValidateTaxonomyCodeThenDoNothing() {
+    String taxonomyCode = "010101P";
+    Long orgId = 1L;
+    String accessToken = "ACCESSTOKEN";
+
+    SendTaxonomy sendTaxonomy = new SendTaxonomy();
+    sendTaxonomy.setTaxonomyCode(taxonomyCode);
+    sendTaxonomy.setOrganizationType("01");
+
+    Organization organization = new Organization();
+    organization.setOrgTypeCode("01");
+
+    Mockito.when(sendTaxonomyRepositoryMock.findByTaxonomyCode(taxonomyCode)).thenReturn(sendTaxonomy);
+    Mockito.when(organizationServiceMock.getOrganization(orgId, accessToken)).thenReturn(organization);
+
+    assertDoesNotThrow(() -> service.validateTaxonomyCode(orgId, taxonomyCode, accessToken));
+  }
+
+  @Test
+  void givenNotPresentTaxonomyCodeWhenValidateTaxonomyCodeThenException() {
+    String taxonomyCode = "010101P";
+    Long orgId = 1L;
+    String accessToken = "ACCESSTOKEN";
+
+    Mockito.when(sendTaxonomyRepositoryMock.findByTaxonomyCode(taxonomyCode)).thenThrow(NotFoundException.class);
+
+    assertThrows(NotFoundException.class, () -> service.validateTaxonomyCode(orgId, taxonomyCode, accessToken));
+  }
+
+  @Test
+  void givenNotPresentOrganizationWhenValidateTaxonomyCodeThenException() {
+    String taxonomyCode = "010101P";
+    Long orgId = 1L;
+    String accessToken = "ACCESSTOKEN";
+
+    SendTaxonomy sendTaxonomy = new SendTaxonomy();
+
+    Mockito.when(sendTaxonomyRepositoryMock.findByTaxonomyCode(taxonomyCode)).thenReturn(sendTaxonomy);
+    Mockito.when(organizationServiceMock.getOrganization(orgId, accessToken)).thenThrow(NotFoundException.class);
+
+    assertThrows(NotFoundException.class, () -> service.validateTaxonomyCode(orgId, taxonomyCode, accessToken));
+  }
+
+  @Test
+  void givenInvalidTaxonomyCodeForOrganizationWhenValidateTaxonomyCodeThenException() {
+    String taxonomyCode = "010101P";
+    Long orgId = 1L;
+    String accessToken = "ACCESSTOKEN";
+
+    SendTaxonomy sendTaxonomy = new SendTaxonomy();
+    sendTaxonomy.setTaxonomyCode(taxonomyCode);
+
+    Organization organization = new Organization();
+    organization.setOrgTypeCode("03");
+
+    Mockito.when(sendTaxonomyRepositoryMock.findByTaxonomyCode(taxonomyCode)).thenReturn(sendTaxonomy);
+    Mockito.when(organizationServiceMock.getOrganization(orgId, accessToken)).thenReturn(organization);
+
+    assertThrows(InvalidTaxonomyException.class, () -> service.validateTaxonomyCode(orgId, taxonomyCode, accessToken));
+  }
+}
