bd3d5a4be5402b3e9918aa101579c73307f1911f
antocalo <antonio.calo@nttdata.com>
2026-05-20 12:18:57 +0200

    P4ADEV-4659 implements tests

--- src/main/java/it/gov/pagopa/pu/send/repository/SendTaxonomyRepository.java
+++ src/main/java/it/gov/pagopa/pu/send/repository/SendTaxonomyRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface SendTaxonomyRepository extends MongoRepository<SendTaxonomy, String>, SendTaxonomyRepositoryExt {
+public interface SendTaxonomyRepository extends MongoRepository<SendTaxonomy, String> {
+  SendTaxonomy findByTaxonomyCode(String taxonomyCode);
 }
--- src/main/java/it/gov/pagopa/pu/send/repository/SendTaxonomyRepositoryExt.java
+++
@@ -1,7 +0,0 @@
-package it.gov.pagopa.pu.send.repository;
-
-import it.gov.pagopa.pu.send.model.SendTaxonomy;
-
-public interface SendTaxonomyRepositoryExt {
-  SendTaxonomy findByTaxonomyCode(String taxonomyCode);
-}
