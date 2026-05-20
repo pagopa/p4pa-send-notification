b5094f8d717d2f65e920b50f0b97cd7859f953d0
Antonio Calò <antonio.calo@nttdata.com>
2026-05-20 13:44:56 +0200

    feat: P4ADEV-4659-implements-taxonomycode-validator (#158)

---
+++ src/main/java/it/gov/pagopa/pu/send/service/TaxonomyValidatorServiceImpl.java
@@ -0,0 +1,43 @@
+package it.gov.pagopa.pu.send.service;
+
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.send.connector.organization.service.OrganizationService;
+import it.gov.pagopa.pu.send.exception.InvalidTaxonomyException;
+import it.gov.pagopa.pu.send.exception.NotFoundException;
+import it.gov.pagopa.pu.send.model.SendTaxonomy;
+import it.gov.pagopa.pu.send.repository.SendTaxonomyRepository;
+import it.gov.pagopa.pu.send.util.ErrorCodeConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class TaxonomyValidatorServiceImpl implements TaxonomyValidatorService {
+  private final SendTaxonomyRepository sendTaxonomyRepository;
+  private final OrganizationService organizationService;
+
+  public TaxonomyValidatorServiceImpl(SendTaxonomyRepository sendTaxonomyRepository, OrganizationService organizationService) {
+    this.sendTaxonomyRepository = sendTaxonomyRepository;
+    this.organizationService = organizationService;
+  }
+
+  @Override
+  public void validateTaxonomyCode(Long organizationId, String taxonomyCode, String accessToken) {
+    SendTaxonomy sendTaxonomy = sendTaxonomyRepository.findByTaxonomyCode(taxonomyCode);
+    if(sendTaxonomy==null) {
+      throw new NotFoundException(ErrorCodeConstants.ERROR_CODE_TAXONOMY_CODE_NOT_FOUND,
+        String.format("Send Taxonomy having taxonomyCode %s not found", taxonomyCode));
+    }
+
+    Organization org = organizationService.getOrganization(organizationId, accessToken);
+    if(org == null) {
+      throw new NotFoundException(ErrorCodeConstants.ERROR_CODE_ORGANIZATION_NOT_FOUND,
+        String.format("Organization having orgId %s not found", organizationId));
+    }
+
+    String orgTypeCode = org.getOrgTypeCode();
+    if (orgTypeCode != null && !orgTypeCode.equals(sendTaxonomy.getOrganizationType())) {
+      throw new InvalidTaxonomyException(String.format("The taxonomyCode [%s] is not valid for the organization type [%s]", taxonomyCode, orgTypeCode));
+    }
+  }
+}
