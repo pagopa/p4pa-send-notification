82d190949ec81beb5f66b1c4a8228f199fc6ba66
MScarsella <marco.scarsella@pagopa.it>
2026-06-22 17:01:40 +0200

    fix: P4ADEV-4771 Campaign entity (#169)

---
+++ src/main/java/it/gov/pagopa/pu/send/model/Campaign.java
@@ -0,0 +1,37 @@
+package it.gov.pagopa.pu.send.model;
+
+import it.gov.pagopa.pu.send.dto.Counters;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@Document("campaign")
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@FieldNameConstants
+@EqualsAndHashCode(callSuper = false)
+public class Campaign extends BaseEntity {
+  @Id
+  private String campaignId;
+  @NotEmpty
+  private String externalId;
+  private String campaignName;
+  @NotNull
+  private Long organizationId;
+  private String orgSubUnitCode;
+  @NotNull
+  @Builder.Default
+  private Counters counters = new Counters();
+  @NotNull
+  private LocalDate startDate;
+  @NotNull
+  private LocalDate endDate;
+}
