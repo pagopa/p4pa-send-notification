ae406e69814946ff4379270f6880db042635ceae
MScarsella <marco.scarsella@pagopa.it>
2026-06-22 09:53:49 +0200

    fix: P4ADEV-4784 fixed PdndApiClient.getVoucherToken (#168)

--- src/test/java/it/gov/pagopa/pu/send/connector/pdnd/client/PdndApiClientTest.java
+++ src/test/java/it/gov/pagopa/pu/send/connector/pdnd/client/PdndApiClientTest.java
@@ -1,10 +1,8 @@
 package it.gov.pagopa.pu.send.connector.pdnd.client;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import it.gov.pagopa.pu.pdnd.client.generated.P4paPdndApi;
 import it.gov.pagopa.pu.pdnd.dto.generated.PdndAuthData;
-import it.gov.pagopa.pu.pdnd.dto.generated.PdndServicesEnum;
+import it.gov.pagopa.pu.pdnd.dto.generated.PdndServiceType;
 import it.gov.pagopa.pu.send.connector.pdnd.config.PagopaPdndApisHolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +12,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @ExtendWith(MockitoExtension.class)
 class PdndApiClientTest {
@@ -36,14 +37,16 @@ class PdndApiClientTest {
     String accessToken = "accessToken";
     PdndAuthData authData = new PdndAuthData();
     authData.setAccessToken(accessToken);
+    Long organizationId = 1L;
+    String subUnitCode = "subUnitCode";
 
     Mockito.when(apisHolder.getP4paPdndApiByApiKey(accessToken))
       .thenReturn(p4paPdndApiMock);
-    Mockito.when(p4paPdndApiMock.getVoucherToken(PdndServicesEnum.SEND))
+    Mockito.when(p4paPdndApiMock.getVoucherToken(PdndServiceType.SEND, organizationId, subUnitCode))
       .thenReturn(authData);
 
     // When
-    PdndAuthData result = pdndApiClient.getVoucherToken(accessToken);
+    PdndAuthData result = pdndApiClient.getVoucherToken(accessToken, organizationId, subUnitCode);
 
     // Then
     assertSame(accessToken, result.getAccessToken());
@@ -53,15 +56,17 @@ class PdndApiClientTest {
   void givenNotExistentServiceWhenGetP4paPdndApiByApiKeyThenReturnNull() {
     // Given
     String accessToken = "accessToken";
+    Long organizationId = 1L;
+    String subUnitCode = "subUnitCode";
 
     Mockito.when(apisHolder.getP4paPdndApiByApiKey(accessToken))
       .thenReturn(p4paPdndApiMock);
-    Mockito.when(p4paPdndApiMock.getVoucherToken(PdndServicesEnum.SEND))
+    Mockito.when(p4paPdndApiMock.getVoucherToken(PdndServiceType.SEND, organizationId, subUnitCode))
       .thenThrow(
         HttpClientErrorException.create(HttpStatus.NOT_FOUND, "NotFound", null, null, null));
 
     // When
-    PdndAuthData result = pdndApiClient.getVoucherToken(accessToken);
+    PdndAuthData result = pdndApiClient.getVoucherToken(accessToken, organizationId, subUnitCode);
 
     // Then
     assertNull(result);
