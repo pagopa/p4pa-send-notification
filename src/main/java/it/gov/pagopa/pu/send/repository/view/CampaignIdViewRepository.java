39185b0dee0ae4b45fa0a06690e13bae20f3bd31
michele.mazzini <michele.mazzini1996@gmail.com>
2026-06-25 12:57:25 +0200

    fix: remove imports

--- src/main/java/it/gov/pagopa/pu/send/repository/view/CampaignIdViewRepository.java
+++ src/main/java/it/gov/pagopa/pu/send/repository/view/CampaignIdViewRepository.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.pu.send.repository.view;
 
-import it.gov.pagopa.pu.send.model.Campaign;
 import it.gov.pagopa.pu.send.model.view.CampaignIdView;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/it/gov/pagopa/pu/send/service/CampaignServiceImpl.java
+++ src/main/java/it/gov/pagopa/pu/send/service/CampaignServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
