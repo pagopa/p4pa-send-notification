e7d9722e38617411bd272220fcfaeb82f7615571
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2026-06-25 15:34:03 +0200

    feat: P4ADEV-4829 add fetch all campaign ids api (#175)

---
+++ src/main/java/it/gov/pagopa/pu/send/controller/CampaignController.java
@@ -0,0 +1,26 @@
+package it.gov.pagopa.pu.send.controller;
+
+import it.gov.pagopa.pu.send.controller.generated.CampaignApi;
+import it.gov.pagopa.pu.send.service.CampaignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class CampaignController implements CampaignApi {
+  private final CampaignService campaignService;
+
+  public CampaignController(CampaignService campaignService) {
+    this.campaignService = campaignService;
+  }
+
+  @Override
+  public ResponseEntity<List<String>> fetchAllCampaignIds() {
+    log.info("retrieve all campaign ids");
+    return new ResponseEntity<>(campaignService.fetchAllIds(), HttpStatus.OK);
+  }
+}
