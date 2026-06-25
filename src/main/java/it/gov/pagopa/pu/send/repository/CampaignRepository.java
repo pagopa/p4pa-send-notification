611d460a5c0f48a7c4dfdb34da0af9d801f78f4c
michele.mazzini <michele.mazzini1996@gmail.com>
2026-06-25 17:28:28 +0200

    refactor: remove unnecessary repository

--- src/main/java/it/gov/pagopa/pu/send/repository/CampaignRepository.java
+++ src/main/java/it/gov/pagopa/pu/send/repository/CampaignRepository.java
@@ -1,12 +1,15 @@
 package it.gov.pagopa.pu.send.repository;
 
 import it.gov.pagopa.pu.send.model.Campaign;
+import it.gov.pagopa.pu.send.model.view.CampaignIdView;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CampaignRepository extends MongoRepository<Campaign, String> {
   Optional<Campaign> findByExternalIdAndOrganizationIdAndOrgSubUnitCode(String externalId, Long organizationId, String orgSubUnitCode);
+  List<CampaignIdView> findAllCampaignIdsBy();
 }
