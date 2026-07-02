c04d4b5fef7a7d61948a3a4b6ca28f3786d12304
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2026-07-02 10:33:42 +0200

    fix: P4ADEV-4792 optimize campaign utils (#183)

--- src/main/java/it/gov/pagopa/pu/send/util/CampaignUtils.java
+++ src/main/java/it/gov/pagopa/pu/send/util/CampaignUtils.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.pu.send.util;
 import it.gov.pagopa.pu.send.connector.send.generated.dto.TimelineElementCategoryV27DTO;
 import it.gov.pagopa.pu.send.dto.Counters;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,7 +11,7 @@ import java.util.stream.Collectors;
 public class CampaignUtils {
   private CampaignUtils() {}
 
-  public static final Map<TimelineElementCategoryV27DTO, Set<String>> COUNTERS_STATUS_RELATION_MAP = Map.of(
+  public static final Map<TimelineElementCategoryV27DTO, Set<String>> TIMELINE_ELEMENT_CATEGORY2COUNTER_FIELDS = Map.of(
     TimelineElementCategoryV27DTO.REQUEST_ACCEPTED, Set.of(Counters.Fields.accepted),
     TimelineElementCategoryV27DTO.DIGITAL_SUCCESS_WORKFLOW, Set.of(Counters.Fields.accepted, Counters.Fields.delivered),
     TimelineElementCategoryV27DTO.ANALOG_SUCCESS_WORKFLOW, Set.of(Counters.Fields.accepted, Counters.Fields.delivered),
@@ -20,10 +21,14 @@ public class CampaignUtils {
     TimelineElementCategoryV27DTO.SEND_DIGITAL_FEEDBACK, Set.of(Counters.Fields.accepted, Counters.Fields.delivered, Counters.Fields.digitalCompleted)
   );
 
-  public static Set<TimelineElementCategoryV27DTO> getStreamEventStatusesForCounter(String counterName) {
-    return COUNTERS_STATUS_RELATION_MAP.entrySet().stream()
-      .filter(entry -> entry.getValue().contains(counterName))
-      .map(Map.Entry::getKey)
-      .collect(Collectors.toSet());
-  }
+  public static final Map<String, Set<TimelineElementCategoryV27DTO>> COUNTER_FIELD2TIMELINE_ELEMENT_CATEGORIES =
+    Collections.unmodifiableMap(
+      TIMELINE_ELEMENT_CATEGORY2COUNTER_FIELDS.entrySet().stream()
+        .flatMap(entry -> entry.getValue().stream()
+          .map(counterName -> Map.entry(counterName, entry.getKey())))
+        .collect(Collectors.groupingBy(
+          Map.Entry::getKey,
+          Collectors.mapping(Map.Entry::getValue, Collectors.toSet())
+        ))
+    );
 }
