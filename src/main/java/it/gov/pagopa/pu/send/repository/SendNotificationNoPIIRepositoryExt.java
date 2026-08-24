62324da07fce88d27ff16dbbe84d230888b81938
michele.mazzini <michele.mazzini1996@gmail.com>
2026-08-24 15:58:39 +0200

    fix: remove unused imports

--- src/main/java/it/gov/pagopa/pu/send/repository/SendNotificationNoPIIRepositoryExt.java
+++ src/main/java/it/gov/pagopa/pu/send/repository/SendNotificationNoPIIRepositoryExt.java
@@ -2,7 +2,6 @@ package it.gov.pagopa.pu.send.repository;
 
 import com.mongodb.client.result.UpdateResult;
 import it.gov.pagopa.pu.send.connector.send.generated.dto.PreLoadResponseDTO;
-import it.gov.pagopa.pu.send.connector.send.generated.dto.TimelineElementCategoryV27DTO;
 import it.gov.pagopa.pu.send.dto.Counters;
 import it.gov.pagopa.pu.send.dto.SendNotificationFiltersDTO;
 import it.gov.pagopa.pu.send.dto.generated.LegalFactDTO;
